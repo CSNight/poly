6c94b85a23ad01e25ebc7dda390575015a3d251e
chensi1 <chensi1@sensetime.com>
2021-05-21 14:55:58 +0800

    fix cors

---
+++ src/main/java/csnight/spider/poly/aop/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package csnight.spider.poly.aop;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Resource
+    private CorsInterceptor corsInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 跨域拦截器需放在最上面
+        registry.addInterceptor(corsInterceptor).addPathPatterns("/**");
+    }
+}
