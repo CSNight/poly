f203a67ea69668ad1d061e658750f7b59530bd02
chensi1 <chensi1@sensetime.com>
2021-05-31 16:22:33 +0800

    remove log

--- src/main/java/csnight/spider/poly/websocket/WebSocketServerHandler.java
+++ src/main/java/csnight/spider/poly/websocket/WebSocketServerHandler.java
@@ -12,8 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.InetSocketAddress;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> {
